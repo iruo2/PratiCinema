fc8cd3a0eaf841499f8f54372a1eebcd4b8f0af8
dSamp41 <d.sampietro@studenti.unipi.it>
2024-03-01 14:08:36 +0100

    Continue rewrite

---
+++ src/Header.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import './Header.css'
+
+export default function Header(){
+    return (
+      <nav className="navbar navbar-expand-lg header">
+        <div className="container-fluid">
+          
+          <Link className="navbar-brand" to="/">PratiCinema</Link>
+          
+          <button className="navbar-toggler" type="button"
+              data-bs-toggle="collapse" data-bs-target="#navbarNav" data-bs-auto-close="true" 
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="navbar-nav">
+                <Link className="nav-link" aria-current="page" to="/rules">Rules</Link>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      </nav>
+    );
+}
