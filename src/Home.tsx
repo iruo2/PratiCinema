592b60325936c2a9535ea80e2f3012eb1bf5b6b6
dSamp41 <d.sampietro@studenti.unipi.it>
2024-03-02 15:56:01 +0100

    Start to show image. Fix dimensions

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,8 @@
+import volantino from './assets/volantino.jpg'
+
 export default function Home(){
     return(<>
         <h1>Benvenuto al PratiCinema!</h1>        
-        
+        <img src={volantino}></img> 
     </>);
 }
